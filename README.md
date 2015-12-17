664066391528b60d92d26b4e8f801e442a56cc3b
Chris Contolini <contolini@users.noreply.github.com>
2015-12-17 17:47:31 -0500

    Add badges to readme

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Capital Framework
+# Capital Framework [![Travis](https://img.shields.io/travis/cfpb/capital-framework.svg?style=flat-square)](https://travis-ci.org/cfpb/capital-framework) [![npm](https://img.shields.io/npm/v/capital-framework.svg?style=flat-square)](https://www.npmjs.com/package/capital-framework)
 
 The Consumer Financial Protection Bureau's user interface framework.
 
--- scripts/travis/publish-capital-framework.sh
+++ scripts/travis/publish-capital-framework.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
 # Abort if this is a pull request OR if we're not on master OR if it's not node v4
-if [[ $TRAVIS_PULL_REQUEST != "false"|| $TRAVIS_BRANCH != $GH_BRANCH || $TRAVIS_NODE_VERSION != "4.2" ]]; then exit 0; fi
+if [[ $TRAVIS_PULL_REQUEST != "false"|| $TRAVIS_BRANCH != $GH_BRANCH || $TRAVIS_NODE_VERSION != "4.2" ]]; then
+  echo "TRAVIS_PULL_REQUEST : ${TRAVIS_PULL_REQUEST}"
+  echo "TRAVIS_BRANCH : ${TRAVIS_BRANCH}"
+  echo "GH_BRANCH : ${GH_BRANCH}"
+  echo "TRAVIS_NODE_VERSION : ${TRAVIS_NODE_VERSION}"
+  exit 0;
+fi
 
 git config user.name "CFPBot"
 git config user.email "CFPBot@users.noreply.github.com"
