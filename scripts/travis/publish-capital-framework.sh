255b469ef3b70a967a740f4858025675521949e8
contolini <contolini@users.noreply.github.com>
2015-12-17 18:26:20 -0500

    Update travis script

--- scripts/travis/publish-capital-framework.sh
+++ scripts/travis/publish-capital-framework.sh
@@ -1,15 +1,26 @@
 #!/bin/sh
 
 # Abort if this is a pull request OR if we're not on master OR if it's not node v4
-if [[ $TRAVIS_PULL_REQUEST != "false"|| $TRAVIS_BRANCH != $GH_BRANCH || $TRAVIS_NODE_VERSION != "4.2" ]]; then
-  echo "TRAVIS_PULL_REQUEST : ${TRAVIS_PULL_REQUEST}"
-  echo "TRAVIS_BRANCH : ${TRAVIS_BRANCH}"
-  echo "GH_BRANCH : ${GH_BRANCH}"
-  echo "TRAVIS_NODE_VERSION : ${TRAVIS_NODE_VERSION}"
+if [ "$TRAVIS_PULL_REQUEST" != "false" ] || [ "$TRAVIS_BRANCH" != "$GH_BRANCH" ] || [ "$TRAVIS_NODE_VERSION" != "4.2" ]; then
+  echo "TRAVIS_PULL_REQUEST: ${TRAVIS_PULL_REQUEST}"
+  echo "TRAVIS_BRANCH: ${TRAVIS_BRANCH}"
+  echo "GH_BRANCH: ${GH_BRANCH}"
+  echo "TRAVIS_NODE_VERSION: ${TRAVIS_NODE_VERSION}"
+  echo "Abort!"
   exit 0;
 fi
 
-git config user.name "CFPBot"
-git config user.email "CFPBot@users.noreply.github.com"
-echo -e "$NPM_USERNAME\n$NPM_PASSWORD\n$NPM_EMAIL" | npm login
-npm run release
+# If `npm whoami` doesn't return anything (meaning the user isn't logged in)
+if [ -z "$(npm whoami)" ]; then
+  # If NPM env variables are set and we're in Travis
+  if [-n "$NPM_USERNAME"] && [-n "$NPM_PASSWORD"] && [-n "$NPM_EMAIL"] && [$TRAVIS == "true"]; then
+    echo -e "$NPM_USERNAME\n$NPM_PASSWORD\n$NPM_EMAIL" | npm login
+    git config user.name "CFPBot"
+    git config user.email "CFPBot@users.noreply.github.com"
+  else
+    echo "Please log into npm before continuing"
+    exit 1
+  fi
+fi
+
+node scripts/npm/prepublish && npm publish
