91a3ec9c3a28780b000ae253c40493b93e0c7cc9
Adam Scott <adam.scott@cfpb.gov>
2016-03-08 15:33:22 -0500

    first pass syntax highlighting

--- gulp/tasks/usage.js
+++ gulp/tasks/usage.js
@@ -4,8 +4,20 @@ var gulp = require( 'gulp' );
 var $ = require( 'gulp-load-plugins' )();
 var config = require( '../config' ).usage;
 var handleErrors = require( '../utils/handleErrors' );
+var pygmentize = require('pygmentize-bundled');
 var path = require( 'path' );
 var fileName;
+
+var markdownOptions = {
+  gfm: true,
+  sanitize: false,
+  highlight: function (code, lang, callback) {
+    pygmentize({ lang: 'html', format: 'html' }, code, function (err, result) {
+      if (err) return callback(err);
+      callback(null, result.toString());
+    });
+  }
+};
 
 // this task takes the usage.md files from the cf-components directory
 // and converts it from markdown to html so that it can be included in
@@ -13,7 +25,7 @@ var fileName;
 gulp.task( 'usage', function() {
   return gulp.src( config.files.src )
     .on( 'error', handleErrors )
-    .pipe( $.markdown() )
+    .pipe( $.markdown( markdownOptions ) )
     .pipe( $.tap( function ( file, t ) {
       fileName = path.dirname( file.path )
         .split( path.sep )
