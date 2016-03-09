7565afff6401df6523b7346056959823afdb1849
Adam Scott <adam.scott@cfpb.gov>
2016-03-09 09:45:39 -0500

    syntax highlighting

--- gulp/tasks/usage.js
+++ gulp/tasks/usage.js
@@ -8,23 +8,34 @@ var pygmentize = require('pygmentize-bundled');
 var path = require( 'path' );
 var fileName;
 
+/**
+ * RegEx to replace GitHub Flavored Markdown backticks
+ * with Pygments syntax
+ */
+var codeStart = new RegExp(/([\`]{3})\n+(\<)/g);
+var codeEnd = new RegExp(/(\>)\n+([\`]{3})/g);
+var startReplace = '{% highlight html %}\n<'
+var endReplace = '>\n{% endhighlight %}'
+
 var markdownOptions = {
   gfm: true,
-  sanitize: false,
   highlight: function (code, lang, callback) {
-    pygmentize({ lang: 'html', format: 'html' }, code, function (err, result) {
+    pygmentize({ lang: 'html', format: 'html', options: { } }, code, function (err, result) {
       if (err) return callback(err);
       callback(null, result.toString());
     });
   }
 };
 
-// this task takes the usage.md files from the cf-components directory
-// and converts it from markdown to html so that it can be included in
-// our Jekyll templates
+/*
+* take the usage.md files from the cf-components directory
+* and convert from markdown to html with syntax highlighting
+*/
 gulp.task( 'usage', function() {
   return gulp.src( config.files.src )
     .on( 'error', handleErrors )
+    .pipe( $.replace( codeStart, startReplace ) )
+    .pipe( $.replace( codeEnd, endReplace ) )
     .pipe( $.markdown( markdownOptions ) )
     .pipe( $.tap( function ( file, t ) {
       fileName = path.dirname( file.path )
