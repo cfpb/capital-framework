4c95b9c80c9a304609469bac643775b8f3a09c91
contolini <contolini@users.noreply.github.com>
2016-06-17 12:30:11 -0400

    Remove unnecessary CF JS snippet

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -7,19 +7,6 @@ global.$ = require( 'jquery' );
 // run `npm install cf-expandables --save` and require() it in this file.
 require( 'cf-expandables' );
 
-// Count all features included in the test page.
-$( '.feature-list' ).append(
-  '<section class="feature-list_item' +
-  ' block block__padded-top block__border-top">' +
-  '<div class="feature-header">' +
-  '<h1 class="feature-header_name">jQuery</h1>' +
-  '</div>' +
-  '<p>jQuery is working and counts a total of ' +
-  '<strong>' + $( '.feature-list_item' ).size() + '</strong> ' +
-  'cf-components.</p>' +
-  '</section>'
-);
-
 if ( $('.cf-download').length ) {
   $.get('https://npmcdn.com/capital-framework/package.json', function( data ) {
     var v = data.version;
