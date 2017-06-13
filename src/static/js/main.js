185b897d07d7166dde299b1a5313ad453e11087f
James Wilson <jimmynotjim@me.com>
2017-06-13 14:08:02 -0500

    Initialize the missing cf-tables scripting
    The docs are not currently including the scripting from cf-tables at all. Updated the main scripts file to correct this.

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -6,6 +6,7 @@ global.$ = require( 'jquery' );
 // If you'd like to include cf-expandables (or any other node module in your project),
 // run `npm install cf-expandables --save` and require() it in this file.
 require( 'cf-expandables' );
+require( 'cf-tables' );
 
 if ( $('.cf-download').length ) {
   $.get('https://npmcdn.com/capital-framework/package.json', function( data ) {
