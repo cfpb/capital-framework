8ef7cb57c0473a2f383a7139993e9a839d835652
Anselm Bradford <Anselm.Bradford@cfpb.gov>
2017-08-30 12:42:33 -0400

    Remove table row links init method

--- src/cf-tables/src/cf-table-row-links.js
+++ src/cf-tables/src/cf-table-row-links.js
@@ -33,21 +33,6 @@ var TableRowLinks = {
     target = closest( event.target, 'tr' );
     var link = target.querySelector( 'a' );
     if( link ) window.location = link.getAttribute( 'href' );
-  },
-
-  /**
-   * Handle initilization of Table Row Links. Added for standalone
-   * use cases.
-   *
-   */
-  init: function() {
-    var elements = document.querySelector( TableRowLinks.ui.base );
-    for ( var i = 0; i < elements.length; ++i ) {
-      if( elements[i].hasAttribute( 'data-bound' ) === false ) {
-        elements[i].addEventListener( 'click', table,
-        TableRowLinks.onRowLinkClick );
-      }
-    }
   }
 };
 
