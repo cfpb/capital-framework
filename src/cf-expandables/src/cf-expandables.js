55b458f57fc507fd58754676a8f36e3e399069eb
Anselm Bradford <Anselm.Bradford@cfpb.gov>
2017-11-17 11:22:48 -0500

    Remove expandable-group accordions

--- src/cf-expandables/src/cf-expandables.js
+++ src/cf-expandables/src/cf-expandables.js
@@ -1,10 +1,8 @@
 'use strict';
 
 const Expandable = require( './Expandable' );
-const ExpandableGroup = require( './ExpandableGroup' );
 
 // polyfill for ie9 compatibility
 require( 'classlist-polyfill' );
 
 Expandable.init();
-ExpandableGroup.init();
